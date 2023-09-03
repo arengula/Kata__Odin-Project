bc322f7fa917746ef669fa09146ba3a8f151911c
Allen <valternatean13@gmail.com>
2023-09-03 20:17:55 +0700

    ref: add factory function for main section content
    The previous code had a lot of copy-pasted code that might lead to harder
    maintenance. The similarities among those code has been converted into a
    factory.
    TODO: Reapply style to elements made from factory functions

--- RestaurantPage/src/components/aboutPage/about.js
+++ RestaurantPage/src/components/aboutPage/about.js
@@ -2,13 +2,59 @@ import "./about.css"
 
 let heroURL = "https://architizer-prod.imgix.net/media/mediadata/uploads/1514377430489Italian-Restaurant-Interior-Design-4.jpg"
 
-const makeHeroRight = () => {
-	let content = document.createElement("div");
+const factSectionGroup = (name, headingText, elements) => {
+	let section = document.createElement("section");
+	let heading = document.createElement("h2");
+	let sectionContent = document.createElement("div");
 
-	return content;
+	section.classList.add("about__sectionGroup");
+	sectionContent.classList.add("about__groupContent");
+	return () => {
+		section.innerHTML = ""
+		heading.textContent = headingText;
+		section.appendChild(heading);
+
+		elements.forEach((elem) => {
+			sectionContent.appendChild(elem);
+		})
+		section.appendChild(sectionContent.cloneNode(true));
+		return section.cloneNode(true);
+	}
 }
 
-const makeHeroLeft = () => {
+const factTextWithImage = (imgURL, headingText, captionText) => {
+	let section = document.createElement("div");
+	let img = document.createElement("img");
+	let heading = document.createElement("h3");
+	let paragraph = document.createElement("p");
+
+	return  () => {
+		section.innerHTML = "";
+		img.src = imgURL;
+		heading.textContent = headingText;
+		paragraph.textContent = captionText;
+
+		section.appendChild(img.cloneNode(true));
+		section.appendChild(heading.cloneNode(true));
+		section.appendChild(paragraph.cloneNode(true));
+		return section.cloneNode(true);
+	}
+}
+
+const makeAbout = () => {
+	return factSectionGroup("about", "About Us", [
+		(factTextWithImage(
+			"https://cdn-icons-png.flaticon.com/512/7064/7064544.png",
+			"Who We Are", "We are the chefs with a passion on sharing abour homeland through our cookings"
+		))(),
+		(factTextWithImage(
+			"https://cdn-icons-png.flaticon.com/512/2720/2720119.png",
+			"What We Do", "We provides various Italian cuisine with our own special touch!"
+		))(),
+	]);
+}
+
+const makeHeroText = () => {
 	let content = document.createElement("div");
 	content.classList.add("content__left");
 
@@ -24,65 +70,10 @@ const makeHeroLeft = () => {
 	return content;
 }
 
-const makeAbout =  () => {
-	let section = document.createElement("section");
-	section.classList.add("about__about", "about__groupContent");
-
-	let tempHeading = document.createElement("h2");
-	let tempParagraph = document.createElement("p");
-	let tempSect = document.createElement("section");
-	tempSect.classList.add("main__entry");
-	let tempImg = document.createElement("img");
-
-	tempImg.src = "https://cdn-icons-png.flaticon.com/512/7064/7064544.png";
-	tempHeading.textContent = "Who We Are";
-	tempParagraph.textContent = "We are the chefs with a passion to share about our homeland through our cookings!"
-	tempSect.appendChild(tempImg.cloneNode(true));
-	tempSect.appendChild(tempHeading.cloneNode(true));
-	tempSect.appendChild(tempParagraph.cloneNode(true));
-	section.appendChild(tempSect.cloneNode(true));
-
-	tempImg.src = "https://cdn-icons-png.flaticon.com/512/2720/2720119.png";
-	tempSect.innerHTML = "";
-	tempHeading.textContent = "What we do";
-	tempParagraph.textContent = "We provide you various Italian cuisine with our own special touch!"
-	tempSect.appendChild(tempImg.cloneNode(true));
-	tempSect.appendChild(tempHeading.cloneNode(true));
-	tempSect.appendChild(tempParagraph.cloneNode(true));
-	section.appendChild(tempSect.cloneNode(true));
-	
-	return section;
-}
-
-const makeSignature = () => {
-	let section = document.createElement("section")
-	return section
-
-}
-
 const makeMainContent = () => {
-	let section = document.createElement("article");
-	
-	let tempGroup = document.createElement("div");
-	let tempHeading = document.createElement("h2");
-	let tempHeadingWrapper = document.createElement("section");
-	tempHeadingWrapper.classList.add("about__groupHeading")
-
-	tempHeading.textContent = "About Us";
-	tempHeadingWrapper.appendChild(tempHeading.cloneNode(true));
-	tempGroup.appendChild(tempHeadingWrapper.cloneNode(true));
-	tempGroup.appendChild(makeAbout());
-	section.appendChild(tempGroup.cloneNode(true))
-
-	tempGroup.innerHTML = "";
-	tempHeadingWrapper.innerHTML = "";
-	tempHeading.textContent = "Our Signature";
-	tempHeadingWrapper.appendChild(tempHeading.cloneNode(true));
-	tempGroup.appendChild(tempHeadingWrapper.cloneNode(true));
-	tempGroup.appendChild(makeSignature());
-	section.appendChild(tempGroup.cloneNode(true));
-
-	return section
+	let section = document.createElement("section");
+	section.appendChild((makeAbout())());
+	return section;
 }
 
 const makeHero = () => {
@@ -95,14 +86,13 @@ const makeHero = () => {
 
 	let heroContent = document.createElement("section");
 	heroContent.classList.add("hero__content");
-	heroContent.appendChild(makeHeroLeft());
-	heroContent.appendChild(makeHeroRight());
+	heroContent.appendChild(makeHeroText());
 	hero.appendChild(heroContent);
 
 	return hero;
 }
 
-const render = () => {
+const renderPage = () => {
 	let section = document.createElement("section")
 
 	let heroSection = makeHero();
@@ -116,4 +106,4 @@ const render = () => {
 	return section
 }
 
-export default render();
+export default renderPage();
