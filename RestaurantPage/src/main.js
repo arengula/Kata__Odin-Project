765d335d899010717a4bd5a97b7408908ba53f22
Allen <valternatean13@gmail.com>
2023-09-04 15:28:23 +0700

    remove main content section
    The function to make subsections was inflexible. A full rewrite would be
    done after this commit
    
    OTHERS: refactor main.js

--- RestaurantPage/src/main.js
+++ RestaurantPage/src/main.js
@@ -3,8 +3,9 @@ import navbar from "./components/nav.js"
 import about from "./components/aboutPage/about.js"
 
 function main() {
-	document.getElementById("root").appendChild(navbar)
-	document.getElementById("root").appendChild(about)
+	[navbar, about].forEach((elem) => {
+		document.getElementById("root").appendChild(elem);
+	})
 }
 
 main()
