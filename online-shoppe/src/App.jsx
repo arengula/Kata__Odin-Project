95ae719c9c301cd4c80b670ebda300c3f5297a8d
Allen <valternatean13@gmail.com>
2024-03-04 19:14:56 +0700

    feat: add site header

--- online-shoppe/src/App.jsx
+++ online-shoppe/src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 
 import './css/App.css'
 import ProductDisplay from "./components/productDisplay"
+import SiteHeading from './components/siteHeading'
 
 function App() {
   return (
     <>
+      <SiteHeading />
       <ProductDisplay />
     </>
   )
