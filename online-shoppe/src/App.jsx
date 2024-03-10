b7f005ad983568cb28ba02bb7ba1cf6b926be89c
Allen <valternatean13@gmail.com>
2024-03-11 06:01:48 +0700

    chore: restart project

--- online-shoppe/src/App.jsx
+++ online-shoppe/src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from 'react'
 
 import './css/App.css'
-import ProductDisplay from "./components/productDisplay"
-import SiteHeading from './components/siteHeading'
 
 function App() {
   return (
     <>
-      <SiteHeading />
-      <ProductDisplay />
     </>
   )
 }
