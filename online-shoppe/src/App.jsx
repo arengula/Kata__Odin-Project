86cceff6fb28bdcd08e1526579831ded1d8921a2
Allen <valternatean13@gmail.com>
2024-03-11 17:19:58 +0700

    ref: move router

--- online-shoppe/src/App.jsx
+++ online-shoppe/src/App.jsx
@@ -1,15 +1,33 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import ProductDisplay from './components/productDisplay.jsx';
 import Navbar from './components/navbar';
+
+const ROUTER = createBrowserRouter( [
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    ),
+    children: [
+      { path: "/", element: <p>Home!</p>, },
+      { path: "shop", element: <ProductDisplay />, },
+      { path: "mycart", element: <p>My Cart!</p>, },
+    ]
+  },
+])
 
 
 function App() {
   const [cart, setCart] = useState([]);
   return (
     <>
-      <Navbar />
-      <Outlet />
+      <RouterProvider router={ROUTER} />
     </>
   )
 }
--- online-shoppe/src/main.jsx
+++ online-shoppe/src/main.jsx
@@ -1,24 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
 import App from './App.jsx'
-import ProductDisplay from './components/productDisplay.jsx';
-
-const ROUTER = createBrowserRouter( [
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      { path: "/", element: <p>Home!</p>, },
-      { path: "shop", element: <ProductDisplay />, },
-      { path: "mycart", element: <p>My Cart!</p>, },
-    ]
-  },
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={ROUTER} />
+    <App />
   </React.StrictMode>,
 )
