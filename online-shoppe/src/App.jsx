58b8863c69c0e17b8df2e1e8f769e4f78541c202
Allen <valternatean13@gmail.com>
2024-03-16 12:40:22 +0700

    feat: add simple homepage

--- online-shoppe/src/App.jsx
+++ online-shoppe/src/App.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ProductDisplay from './components/productDisplay.jsx';
 import MyCart from './components/myCart.jsx';
 import Navbar from './components/navbar';
+import Home from './components/home.jsx';
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -23,7 +24,7 @@ function App() {
         </>
       ),
       children: [
-        { path: "/", element: <p>Home!</p>},
+        { path: "/", element: <Home />},
         { path: "shop", element: <ProductDisplay onItemSelect={addToCart}/>},
         { path: "mycart", element: <MyCart cart={cart} onPurchase={clearCart}/>},
       ]
